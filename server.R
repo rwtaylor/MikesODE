109a43466cfd7e6a35b2c74119937d2b483ea945
Ryan Taylor <ryan@ryantaylor.net>
2019-03-01 17:21:06 -0800

    Big update, now kinda working

--- server.R
+++ server.R
@@ -11,32 +11,55 @@ library(ggthemes)
 shinyServer(function(input, output) {
 
   heterozygosities <- function(times, y, parms){
-    library(deSolve)
-    L = parms[1]; N = parms[2]; mu = parms[3]; m = parms[4];
-    dH_local       = 2*mu*(1-y[1]) + 2*m*(y[2]-y[1]) - y[1]/(2*N)
-    dH_global      = 2*mu*(1-y[2]) - y[1]/(2*N*L)
-    list(c(dH_local, dH_global))
+    L = parms[1]; N = parms[2]; mu = parms[3]; m = parms[4]; rrate = parms[5]; rfrac = parms[6];
+    dH_within  = -y[1]/(2*N) + 2*mu*(1-y[1]) + 2*m*(y[2]-y[1]) + 2*rrate*rfrac*(y[2]-y[1]) - rrate*(rfrac^2)*(2*y[2]-y[1]);
+    dH_general = -y[1]/(2*N*L) + 2*mu*(1-y[2]) - (2*y[2]-y[1]) * rrate * (rfrac^2) / L
+    list(c(dH_within,dH_general))
   }
 
-  trajectories <- function(generations = 3000, L = 5, N = 500, mu = 5e-09, Ntot0 = 1e5, m = 1) {
-    library(dplyr)
-    H0 = 4 * input$Ntot0 * input$mu
-    yini <- c(H0,H0)
+  trajectories <- function(generations = 1:input$generations, L = input$L,
+    N = input$N, mu = input$mu, Ntot0 = input$Ntot0, m = 0,
+    rrate = input$r_rate, rfrac = input$r_frac, yini) {
+    times <- generations - min(generations) + 1
+
     out_m <- ode(
-               times = 1:generations,
+      times = times,
                y = yini,
                func = heterozygosities,
-               parms = c(L, N, mu, m)
+               parms = c(L, N, mu, m, rrate, rfrac)
              )
-    out_df <- data_frame(
-                gens = rep(1:generations, 2),
-                type = rep(c("within", "total"), each = generations),
-                heterozygocity = c(out_m[, 2], out_m[, 3])
+    out_df <- tibble(
+                generation = rep(generations, 2),
+                type = rep(c("within", "total"), each = max(times)),
+                heterozygosity = c(out_m[, 2], out_m[, 3])
               )
     return(out_df)
   }
 
-  colors <- c("No migration" = rgb(7,54,248, maxColorValue = 255), "Full admixture" = rgb(0,0,0, maxColorValue = 255), "One mig per gen" = rgb(45,249,66, maxColorValue = 255))
+  trajectories2 <- function(generations = 1:input$generations, L = input$L,
+    N = input$N, mu = input$mu, Ntot0 = input$Ntot0, m = 0,
+    rrate = input$r_rate, rfrac = input$r_frac, yini) {
+    browser()
+    times <- generations - min(generations) + 1
+    out_m <- ode(
+      times = times,
+               y = yini,
+               func = heterozygosities,
+               parms = c(L, N, mu, m, rrate, rfrac)
+             )
+    out_df <- tibble(
+                generation = rep(generations, 2),
+                type = rep(c("within", "total"), each = max(times)),
+                heterozygosity = c(out_m[, 2], out_m[, 3])
+              )
+    return(out_df)
+  }
+
+
+  colors <- c("No migration" = rgb(7,54,248, maxColorValue = 255),
+              "Full admixture" = rgb(0,0,0, maxColorValue = 255),
+              "One mig per gen" = rgb(45,249,66, maxColorValue = 255),
+              "Het. threshold" = rgb(254, 173, 67, maxColorValue = 255))
 
   migration_calculator <- function(m, N ) {
     # Calculates migration rate from input text
@@ -46,36 +69,102 @@ shinyServer(function(input, output) {
       m == "One mig per gen" ~ 1/N
       )
   }
+  # Scenario functions.
 
-makePlot <- reactive ({
+  s_no_migration <- reactive({
+    teta = 4 * input$Ntot0 * input$mu
+    H0 = teta / (1+teta)
+
+    out <- trajectories(m = 0, rrate = 0, yini = c(H0, H0))
+    out$scenario = "No migration"
+    out
   })
 
-output$distPlot <- renderPlot({
-
-  migration_rates <- migration_calculator(input$migration, input$N)
-
-  plot_data <- foreach(i = 1:length(migration_rates), .combine = rbind) %do% {
-    out <- trajectories(input$generations, input$L, input$N, input$mu, input$Ntot0, migration_rates[i])
-    out$migration = input$migration[i]
-    out$m = migration_rates[i]
+  s_full_admix <- reactive({
+    teta = 4 * input$Ntot0 * input$mu
+    H0 = teta / (1+teta)
+    out <- trajectories(m = 1, rrate = 0, yini = c(H0, H0))
+    out$scenario = "Full admixture"
     out
+  })
+
+  s_ompg <- reactive({
+    teta = 4 * input$Ntot0 * input$mu
+    H0 = teta / (1+teta)
+    out <- trajectories(m = 1/input$N, rrate = 0, yini = c(H0, H0))
+    out$scenario = "One mig per gen"
+    out
+  })
+
+  last_hets <- function(x) {
+    x <- x %>% filter(generation == max(generation))
+    hl <- x %>% filter(type == "within") %>% pull(heterozygosity)
+    hg <- x %>% filter(type == "total") %>% pull(heterozygosity)
+    return(c(hl, hg))
   }
 
-  plot_data$migration <- factor(plot_data$migration, levels = c("No migration","Full admixture","One mig per gen"))
-  if(input$which_het == 1){
-    plot_data <- plot_data %>% filter(type == "within")
-  } else if(input$which_het == 2){
-    plot_data <- plot_data %>% filter(type == "total")
+  s_het_thresh <- reactive({
+
+    teta = 4 * input$Ntot0 * input$mu
+    H0 = teta / (1+teta)
+    out_data <- trajectories(m = 0, rrate = 0, yini = c(H0, H0))
+    gen_h_crit <- out_data %>% filter(type == "within", heterozygosity > input$h_critical) %>% pull(generation) %>% max()
+    out_data <- out_data %>% filter(generation < gen_h_crit)
+
+    while(max(out_data$generation) < input$generations){
+      rescue_gens <- max(out_data$generation):(max(out_data$generation) + 1)
+      rescue_data <- trajectories(generations = rescue_gens, m = 0, rrate = input$r_rate, rfrac = input$r_frac, yini = last_hets(out_data))
+      rescue_data <- rescue_data %>% filter(generation == max(generation))
+      decline_data <- trajectories(generations = max(out_data$generation)+1:input$generations, m = 0, rrate = 0, yini = last_hets(rescue_data))
+      gen_h_crit <- decline_data %>% filter(type == "within", heterozygosity > input$h_critical) %>% pull(generation) %>% max()
+      decline_data <- decline_data %>% filter(generation < gen_h_crit)
+      out_data <- rbind(out_data, rescue_data, decline_data)
+    }
+
+    out_data$scenario = "Het. threshold"
+    out_data
+  })
+
+  scenario_switcher <- function(x){
+    if(x == "No migration") {
+      s_no_migration()
+    } else if (x == "Full admixture") {
+      s_full_admix()
+    } else if (x == "One migrant per generation") {
+      s_ompg()
+    } else if (x == "Heterozygosity threshold") {
+      s_het_thresh()
+    }
   }
 
-  p <- ggplot(plot_data, aes(x = gens, y = heterozygocity, color = migration, linetype = type, size = type)) + geom_line(alpha = 0.9) + xlab("Generation") + ylab("Heterozygosity")
+  get_plot_data <- reactive({
+    H0 = 4 * input$Ntot0 * input$mu / (1 + 4 * input$Ntot0 * input$mu)
+    out <- bind_rows(lapply(input$scenarios, scenario_switcher))
+    out$scenario <- factor(out$scenario, levels = c("No migration","Full admixture","One mig per gen", "Het. threshold", "Pop. decline"))
+    if(input$which_het == 1){
+      out <- out %>% filter(type == "within")
+    } else if(input$which_het == 2){
+      out <- out %>% filter(type == "total")
+    }
+    out
+  })
+
+
+output$distPlot <- renderPlot({
+  plot_data <- get_plot_data()
+
+  p <- ggplot(plot_data, aes(x = generation, y = heterozygosity, color = scenario, linetype = type)) + geom_line(alpha = 0.9, size = 1) + xlab("Generation") + ylab("Heterozygosity")
 
   font_size = 24
   p <- p + theme_minimal(base_size = font_size)
-  p <- p + scale_color_manual(values = colors, name = "Migration") + scale_linetype_manual(values = c("total" = "solid", "within" = "dotted"), name = "Heterozygosity") + scale_size_manual(values = c("total" = 3, "within" = 5), name = "Heterozygosity")
-  p <- p + geom_hline(yintercept = input$h_critical)
-  p <- p + geom_hline(yintercept = input$h_pop_decline)
-#  p <- ggplotly(p)
+  p <- p + scale_color_manual(values = colors, name = "Migration") + scale_linetype_manual(values = c("total" = "solid", "within" = "dashed"), name = "Heterozygosity") + scale_size_manual(values = c("total" = 3, "within" = 5), name = "Heterozygosity")
+  if (any(input$plot_thresholds == "h_critical")) {
+    p <- p + geom_hline(yintercept = input$h_critical, color = 'red', linetype = 'dotted')
+  }
+  if (any(input$plot_thresholds == "h_pop_decline")) {
+    p <- p + geom_hline(yintercept = input$h_pop_decline, color = 'red', linetype = 'dotted')
+  }
+
   p
 
 })
--- ui.R
+++ ui.R
@@ -19,7 +19,7 @@ shinyUI(fluidPage(
             "Individuals (Ne) per deme:",
             min   = 1,
             max   = 10000,
-            value = 500
+            value = 100
             ),
           sliderInput("Ntot0",
             "Original population size:",
@@ -35,52 +35,58 @@ shinyUI(fluidPage(
             ),
           numericInput("mu",
             "Mutation rate:",
-            step = 1e-9,
-            value = 5e-9
+            step = 1e-8,
+            value = 1e-5
             )
         ),
         tabPanel("Scenarios",
-          checkboxGroupInput("migration",
-            label = "Choose scenarios to plot",
+          h3("Choose scenarios to plot"),
+          checkboxGroupInput("scenarios",
+          label = NULL,
             choices = list(
                         "No migration" = "No migration",
                         "Full admixture" = "Full admixture",
-                        "One mig per gen" = "One mig per gen",
-                        "Heterozygosity Threshold" = "Heterozygosity Threshold (Not implemented yet)",
-                        "Population Decline (Not implemented yet)"
+                        "One migrant per generation" = "One migrant per generation",
+                        "Heterozygosity threshold" = "Heterozygosity threshold",
+                        "Population decline (Not implemented yet)" = "Population decline"
                       ),
-            selected = c("No migration", "Full admixture", "One mig per gen")
+            selected = c("No migration", "Full admixture", "One migrant per generation")
           ),
           hr(),
+          h3("Scenario parameters"),
+          p("These parameters are shared across all applicable scenarios."),
           sliderInput("h_critical",
             "Critical threshold of heterozygosity",
             min = 0, max = 1, value = 0.2
           ),
-          hr(),
           sliderInput('h_pop_decline',
             'Heterozygosity threshold for population decline',
             min = 0, max = 1, value = 0.1
           ),
-          hr(),
-          sliderInput('r_proportion',
+          sliderInput('r_rate',
+            'Rescue rate',
+            min = 0, max = 1, value = 1
+          ),
+          sliderInput('r_frac',
             'Proportion of alleles replaced during rescue',
             min = 0, max = 1, value = 0.2
           )
-        ),
+        ), #tabpanel scenarios
         tabPanel("Plot Options",
           radioButtons("which_het", label = h3("Plot Heterozygosity"),
-             choices = list("Local" = 1, "Global" = 2, "Both" = 3), 
+             choices = list("Local" = 1, "Global" = 2, "Both" = 3),
              selected = 3
           ),
           checkboxGroupInput("plot_thresholds",
             label = h3("Thresholds"),
-            choices = list("Plot Thresholds" = TRUE)
+            choices = list("Plot h critical" = "h_critical",
+                           "Plot h population decline" = "h_pop_decline")
           )
-        )
-      )
-    ),
+        ) #tabpanel
+      ) #tabsetpanel
+    ), #column3
     column(9,
-      plotOutput("distPlot")
+      plotOutput("distPlot", height = "800px")
     )
   ),
   fluidRow(
@@ -89,6 +95,8 @@ shinyUI(fluidPage(
         h3("Save Plot"),
         textInput('plot_filename', "OM2M.pdf", label = "Filename"),
         downloadButton('downloadPlot', 'Download Plot')
-        ))
-    )
-))
+        )
+      )
+    )#fluidRow
+  )#fluidPage
+)#shinyui
